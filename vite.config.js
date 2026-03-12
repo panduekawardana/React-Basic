959095670390bf8d62a62b926dd50cad2e25f053
panduekawardana <panduwardanav@gmail.com>
2026-03-12 22:10:24 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/assets/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [react(), tailwindcss()],
 });
